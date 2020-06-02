3243a9d025225744c3d91d5d88cda178b5dbef2d
Kris Penney <kristopher.penney@shopify.com>
2020-06-02 19:07:52 -0400

    Add a way to resolve input ports

--- src/resolve_port.rs
+++ src/resolve_port.rs
@@ -1,4 +1,6 @@
-use midir::{MidiOutput, MidiOutputPort, MidiOutputPorts};
+use midir::{
+    MidiInput, MidiInputPort, MidiInputPorts, MidiOutput, MidiOutputPort, MidiOutputPorts,
+};
 use std::error::Error;
 use std::io::{stdin, stdout, Write};
 
@@ -27,6 +29,36 @@ pub fn midi_output<'a>(
             let port = out_ports
                 .get(input.trim().parse::<usize>()?)
                 .ok_or("invalid output port selected")?;
+            Ok(port)
+        }
+    }
+}
+
+pub fn midi_input<'a>(
+    midi_input: &MidiInput,
+    in_ports: &'a MidiInputPorts,
+) -> Result<&'a MidiInputPort, Box<dyn Error>> {
+    match in_ports.len() {
+        0 => Err("no input port found".into()),
+        1 => {
+            println!(
+                "Choosing the only available input port: {}",
+                midi_input.port_name(&in_ports[0]).unwrap()
+            );
+            Ok(&in_ports[0])
+        }
+        _ => {
+            println!("\nAvailable input ports:");
+            for (i, p) in in_ports.iter().enumerate() {
+                println!("{}: {}", i, midi_input.port_name(p).unwrap());
+            }
+            print!("Please select input port: ");
+            stdout().flush()?;
+            let mut input = String::new();
+            stdin().read_line(&mut input)?;
+            let port = in_ports
+                .get(input.trim().parse::<usize>()?)
+                .ok_or("invalid input port selected")?;
             Ok(port)
         }
     }
